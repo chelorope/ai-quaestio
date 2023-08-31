88a2a92b78413c633c77326cf0756df44f15004d
chelorope <cmarcelorp@gmail.com>
2023-08-31 15:25:38 -0300

    API response structure updates

---
+++ Web/QuaestioAPI/src/main/java/com/web/quaestio/FactsMap.java
@@ -0,0 +1,5 @@
+package com.web.quaestio;
+
+import java.util.HashMap;
+
+public class FactsMap extends HashMap<String, QuestionaireFact> {}
---
+++ Web/QuaestioAPI/src/main/java/com/web/quaestio/QuestionsMap.java
@@ -0,0 +1,5 @@
+package com.web.quaestio;
+
+import java.util.HashMap;
+
+public class QuestionsMap extends HashMap<String, QuestionaireQuestion> {}
